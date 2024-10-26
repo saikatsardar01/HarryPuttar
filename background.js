8c552a68c10a969195edebd7d70b403cdaace9f9
sagario9 <saikatsardar180@gmail.com>
2024-10-26 19:57:50 +0530

    commit

--- background.js
+++ background.js
@@ -1,8 +1,10 @@
 const images = [
     'url("./Assect/33731-1920x1080-desktop-1080p-harry-potter-background-image.jpg")',
     'url("./Assect/248173-1920x1081-desktop-hd-hogwarts-background-image.jpg")',
-    // 'url("image3.jpg")',
-    // 'url("image4.jpg")'
+    'url("./Assect/248165-1920x1080-desktop-1080p-hogwarts-background.jpg")',
+    'url("./Assect/133366-2000x1191-desktop-hd-harry-potter-wallpaper.jpg")',
+    'url("./Assect/248169-1920x1080-desktop-full-hd-hogwarts-wallpaper-photo.jpg")',
+    'url("./Assect/248234-3840x2160-desktop-4k-hogwarts-background.jpg")'
 ];
 
 let currentIndex = 0;
