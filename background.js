6e62cf58eccf0141a4c95aa711d895bcbec094b8
sagario9 <saikatsardar180@gmail.com>
2024-10-26 20:20:36 +0530

    Last Update

--- background.js
+++ background.js
@@ -1,8 +1,8 @@
 const images = [
     'url("./Assect/33731-1920x1080-desktop-1080p-harry-potter-background-image.jpg")',
-    'url("./Assect/248173-1920x1081-desktop-hd-hogwarts-background-image.jpg")',
-    'url("./Assect/248165-1920x1080-desktop-1080p-hogwarts-background.jpg")',
     'url("./Assect/133366-2000x1191-desktop-hd-harry-potter-wallpaper.jpg")',
+    'url("./Assect/248165-1920x1080-desktop-1080p-hogwarts-background.jpg")',
+    'url("./Assect/248173-1920x1081-desktop-hd-hogwarts-background-image.jpg")',
     'url("./Assect/248169-1920x1080-desktop-full-hd-hogwarts-wallpaper-photo.jpg")',
     'url("./Assect/248234-3840x2160-desktop-4k-hogwarts-background.jpg")'
 ];
@@ -11,6 +11,7 @@ let currentIndex = 0;
 const changeBackground = () => {
     const container = document.querySelector('body');
     container.style.backgroundImage = images[currentIndex];
+    container.style.transition = 'background-image 2s ease-in-out';
     currentIndex = (currentIndex + 1) % images.length;
 };
 
